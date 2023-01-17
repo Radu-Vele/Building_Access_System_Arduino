fabbbfc92cd77418a0069d73d73fa4b2e0028fbe
radu_vele <99049451+Radu-Vele@users.noreply.github.com>
2023-01-17 22:32:54 +0200

    added bluetooth access for user

--- src/main/main.ino
+++ src/main/main.ino
@@ -23,6 +23,11 @@ char keymap[numKeypadRows][numKeypadCols] = {
 {'*', '0', '#', 'D'}
 };
 
+//Bluetooth
+String bluetoothCommandReceived = "";
+bool bluetoothCommandComplete = false;
+bool adminMode = false;
+
 /* OBJECTS */
 LiquidCrystal_I2C lcd(I2C_ADDR, 16, 2);
 MFRC522 mfrc522(RFID_SS_PIN, RFID_RST_PIN); //RFID object
@@ -61,6 +66,10 @@ void setup() {
   //Servo init
   myServo.attach(SERVO_PIN);
   myServo.write(SERVO_CLOSE_ANGLE);
+
+  //Bluetooth init
+  Serial1.begin(9600);
+  bluetoothCommandReceived.reserve(200);
 }
 
 void writeLineToLcd(int lineNr, String text, bool clscr) {
@@ -97,7 +106,14 @@ void allowAccess() {
   myServo.write(SERVO_OPEN_ANGLE);
   delay(SERVO_DELAY);     
   myServo.write(SERVO_CLOSE_ANGLE);
+}
+
+void allowRemoteAccess() {
   writeLineToLcd(0, "Authorized access", true);
+  writeLineToLcd(1, "Remote command!", false);
+  myServo.write(SERVO_OPEN_ANGLE);
+  delay(SERVO_DELAY);     
+  myServo.write(SERVO_CLOSE_ANGLE);
 }
 
 void setWelcomeMessage() {
@@ -105,42 +121,88 @@ void setWelcomeMessage() {
     writeLineToLcd(1, WELCOME_MSG_L2, false);
 }
 
-void loop() {  
-  checkSleepMode();
-  
-  if(awake == 0) {
-    checkProximity(); //need to check only if awake is 0, the sleep mode is induced by internal timers  
-  }
-  else { //only check for card or key if someone is close
-    String uid = processRFID(mfrc522);
-    if(!uid.equals("")) {
-      if(presentInUIDArray(uid)) {
-        prvMillisSleep = millis();
-        allowAccess();
-        setWelcomeMessage();
-      }
-      else {
-        prvMillisSleep = millis();
-        writeLineToLcd(0, "Access denied", true);
-        delay(SERVO_DELAY);
-        setWelcomeMessage();
-      }
+void serialEvent1() {
+  String bluetoothInput;
+  bluetoothInput = "";
+  while(Serial1.available()) {
+    char inChar = (char)Serial1.read();
+    bluetoothCommandReceived += inChar;
+    Serial.println(inChar);
+    if(inChar == '\n') {
+      bluetoothCommandComplete = true;
     }
+  }
+}
 
-    char key = myKeypad.getKey();
-    if(key) {
-      String keypadCode = retrieveKeyCode(myKeypad, key);
-      if(presentInKeyCodeArray(keypadCode)) {
-        prvMillisSleep = millis();
-        allowAccess();
-        setWelcomeMessage();
+void parseBluetoothCommand() {
+  if(bluetoothCommandReceived.startsWith("{user ")) {
+    allowRemoteAccess();
+    setWelcomeMessage();
+  }
+  else if(bluetoothCommandReceived.startsWith("{admin ")) {
+    adminMode = true;
+  }
+}
+
+void checkBluetoothCommand() {
+    if(bluetoothCommandComplete) {
+      Serial.println(bluetoothCommandReceived);
+      parseBluetoothCommand();
+      bluetoothCommandComplete = false;
+      bluetoothCommandReceived = "";
+    }
+}
+
+void userModeFlow() {
+    checkBluetoothCommand();
+    checkSleepMode();
+    
+    if(awake == 0) {
+      checkProximity(); //need to check only if awake is 0, the sleep mode is induced by internal timers  
+    }
+    else { //only check for card or key if someone is close
+      String uid = processRFID(mfrc522);
+      if(!uid.equals("")) {
+        if(presentInUIDArray(uid)) {
+          prvMillisSleep = millis();
+          allowAccess();
+          setWelcomeMessage();
+        }
+        else {
+          prvMillisSleep = millis();
+          writeLineToLcd(0, "Access denied", true);
+          delay(SERVO_DELAY);
+          setWelcomeMessage();
+        }
       }
-      else {
-        prvMillisSleep = millis();
-        writeLineToLcd(0, "Access denied", true);
-        delay(SERVO_DELAY);
-        setWelcomeMessage();
+  
+      char key = myKeypad.getKey();
+      if(key) {
+        String keypadCode = retrieveKeyCode(myKeypad, key);
+        if(presentInKeyCodeArray(keypadCode)) {
+          prvMillisSleep = millis();
+          allowAccess();
+          setWelcomeMessage();
+        }
+        else {
+          prvMillisSleep = millis();
+          writeLineToLcd(0, "Access denied", true);
+          delay(SERVO_DELAY);
+          setWelcomeMessage();
+        }
       }
     }
+}
+
+void adminModeFlow() {
+  checkBluetoothCommand();
+}
+
+void loop() {
+  if(adminMode) {
+    adminModeFlow();
   }
+  else {
+    userModeFlow();
+  } 
 }
